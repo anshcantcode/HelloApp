08bf70b69c7af3ffe7c83587c63134a948a0675a
anshcantcode <anshgolcha7@gmail.com>
2026-02-10 13:38:16 +0530

    feature(UC1): print literal text OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,20 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC1: Print literal text "OOPS" to the console.
+ * This is the entry point for the OOPS Banner Application.
+ *
+ * @author Vansh
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method – application entry point
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
