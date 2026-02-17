269f3039d68b3382ffe8937b7c5dc60cf523ada7
anshcantcode <anshgolcha7@gmail.com>
2026-02-17 12:57:54 +0530

    feature(UC1): print OOPS to console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,28 +1,14 @@
 /**
  * OOPSBannerApp
  *
- * UC4: Display "OOPS" banner using array and loop for better modularity.
+ * UC1: Print literal text "OOPS" to the console.
  *
  * @author Vansh
- * @version 4.0
+ * @version 1.0
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-
-        String[] banner = {
-                String.join(" ", "*****", "*****", "*****", "*****"),
-                String.join(" ", "*   *", "*   *", "*   *", "*   *"),
-                String.join(" ", "*   *", "*   *", "*   *", "*   *"),
-                String.join(" ", "*   *", "*   *", "*   *", "*   *"),
-                String.join(" ", "*   *", "*   *", "*   *", "*   *"),
-                String.join(" ", "*   *", "*   *", "*   *", "*   *"),
-                String.join(" ", "*****", "*****", "*****", "*****")
-        };
-
-        // Loop to print banner
-        for (String line : banner) {
-            System.out.println(line);
-        }
+        System.out.println("OOPS");
     }
 }
