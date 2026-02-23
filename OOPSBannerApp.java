eff76cb6494238faa120db4107fc5045a4d9f5d0
Ansh <anshgolcha7@gmail.com>
2026-02-23 13:13:26 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,14 @@
-/**
- * OOPSBannerApp
- *
- * UC2: Display "OOPS" as a large banner using '*' characters.
- *
- * @author Vansh
- * @version 2.0
- */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****   *****   *****   ***** ");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println("*     * *     * *     * *     *");
-        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println(" *****   *****   *****    ***** ");
+        System.out.println("*     * *     * *     *  *     *");
+        System.out.println("*     * *     * *     *  *      ");
+        System.out.println("*     * *     * *****    *****  ");
+        System.out.println("*     * *     * *             * ");
+        System.out.println("*     * *     * *      *     *  ");
+        System.out.println(" *****   *****  *       *****   ");
 
     }
 }
