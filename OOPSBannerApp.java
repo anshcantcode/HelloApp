c4d02e7b806865055787334c73ade19d18b97333
Ansh <anshgolcha7@gmail.com>
2026-02-23 13:14:50 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,23 +1,15 @@
-/**
- * OOPSBannerApp
- *
- * UC4: Display "OOPS" banner using array and loop for better modularity.
- *
- * @author Vansh
- * @version 4.0
- */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
         String[] banner = {
                 String.join(" ", "*****", "*****", "*****", "*****"),
-                String.join(" ", "*   *", "*   *", "*   *", "*   *"),
-                String.join(" ", "*   *", "*   *", "*   *", "*   *"),
-                String.join(" ", "*   *", "*   *", "*   *", "*   *"),
-                String.join(" ", "*   *", "*   *", "*   *", "*   *"),
-                String.join(" ", "*   *", "*   *", "*   *", "*   *"),
-                String.join(" ", "*****", "*****", "*****", "*****")
+                String.join(" ", "*   *", "*   *", "*   *", "*    "),
+                String.join(" ", "*   *", "*   *", "*   *", "*    "),
+                String.join(" ", "*   *", "*   *", "*****", "*****"),
+                String.join(" ", "*   *", "*   *", "*    ", "    *"),
+                String.join(" ", "*   *", "*   *", "*    ", "    *"),
+                String.join(" ", "*****", "*****", "*    ", "*****")
         };
 
         // Loop to print banner
